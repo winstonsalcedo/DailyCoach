10b3c768420bf7d50a3344b10eeca153aefaa566
Winston Salcedo <winstonsalcedo@gmail.com>
2020-01-09 11:11:33 -0500

    Deleted Storyboard reference and its references, created a programmatic window

---
+++ Daily Coach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Coach
+//
+//  Created by Winston Salcedo on 1/9/20.
+//  Copyright © 2020 Evolve Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
